1acc1103c0fcdf42f9a55849caad10ae3d6fe047
Desiree Ann Guzman <desireeann000@gmail.com>
2024-09-07 18:37:04 +0800

    Initial commit with app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log('Hello, World!');
+let hername = "Desiree Ann P. Guzman";
+console.log('My name is ' + hername);
+
+console.log('USING VARIABLES');
+let name = "Desiree Ann Pabellano Guzman";
+let age = "19";
+
+console.log("Hello, World! I am " + name + " and I am " + age + "years old.");
+
+console.log("USING NUMBERS");
+const x =  15;
+const y = 12;
+
+const quotient = x / y;
+console.log(quotient);
+
+console.log("USING BOOLEANS");
+let a = Boolean ('');
+let b = Boolean ('real!');
+console.log("Am I giving up? "+ a);
+console.log("Am I starting to love programming? " + b);
+
+console.log("USING COMPARISON OPERATORS");
+let d = 12
+let g = 15
+
+let c = d > g;
+console.log("Is d bigger in comparison to g? "+ c);
+let f = d < g;
+console.log("Is d smaller in comparison to g? "+ f);
+let h = d === g;
+console.log("Are d and g the same? "+ h);
+let i = d != g;
+console.log("Does d differ from g? "+ i);
