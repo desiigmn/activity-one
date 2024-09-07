2b5c12b7f347078bb27965473d7814021451f210
Desiree Ann Guzman <desireeann000@gmail.com>
2024-09-07 19:12:56 +0800

    completed variables section

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log('My name is ' + hername);
 console.log('VARIABLES SECTION');
 let name = "Desiree Ann Pabellano Guzman";
 let age = "nineteen ";
-let address = "Villa Quirino, San Esteban, Ilocos Sur"
+let address = "Villa Quirino, San Esteban, Ilocos Sur";
 
 console.log("Hello, World! I am " + name + " and I am " + age + "years old. " + "From " + address + '.');
 
@@ -23,8 +23,8 @@ console.log("Am I giving up? "+ a);
 console.log("Am I starting to love programming? " + b);
 
 console.log('COMPARISON OPERATORS SECTION');
-let d = 12
-let g = 15
+let d = 12;
+let g = 15;
 
 let c = d > g;
 console.log("Is d bigger in comparison to g? "+ c);
