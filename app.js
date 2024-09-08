ad90dcd13ea542a82af13ae32ec016110d9e2467
Desiree Ann Guzman <desireeann000@gmail.com>
2024-09-08 14:28:00 +0800

    Completed Variables Section

--- app.js
+++ app.js
@@ -2,27 +2,36 @@ console.log('Hello, World!');
 let hername = "Desiree Ann P. Guzman";
 console.log('My name is ' + hername);
 
-console.log('VARIABLES SECTION');
+console.log('***VARIABLES SECTION***');
 let name = "Desiree Ann Pabellano Guzman";
 let age = "nineteen";
 let address = "Villa Quirino, San Esteban, Ilocos Sur";
 
 console.log("Hello, World! I am " + name + " and I am " + age + " years old. " + "From " + address + '.');
 
-console.log("NUMBERS SECTION");
+console.log("***NUMBERS SECTION***");
 const x =  15;
 const y = 12;
+
+const sum = x + y;
+console.log(sum);
+
+const difference = x - y;
+console.log(difference);
+
+const product = x * y;
+console.log(product);
 
 const quotient = x / y;
 console.log(quotient);
 
-console.log('BOOLEANS SECTION');
+console.log('***BOOLEANS SECTION***');
 let a = Boolean ('');
 let b = Boolean ('real!');
 console.log("Am I giving up? "+ a);
 console.log("Am I starting to love programming? " + b);
 
-console.log('COMPARISON OPERATORS SECTION');
+console.log('***COMPARISON OPERATORS SECTION***');
 let d = 12;
 let g = 15;
 
